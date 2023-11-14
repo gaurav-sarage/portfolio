25a1b3377314fd5499bf9049d52b98cdcb25ace3
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-11-14 17:49:38 +0530

    imported PageSEO and designed 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+import { BsArrowLeftShort } from "react-icons/bs";
+
+import { PageSEO } from "@/components/SEO";
+
+import styles from "@/styles/404.module.css";
+import cat from "@/public/cat.jpg";
+
+export default function FourOhFourPage() {
+    return (
+        <>
+            <PageSEO
+                title="404 - Page Not Found | Gaurav Sarage"
+                description="Page Not Found"
+            />
+            <div className={styles.container}>
+                <div style={{ width: "60%", height: "auto", margin: "1rem auto"}}>
+                    <Image src={cat} alt="Cat Image" />
+                </div>
+                <h1>Oh no, you found the cat 😸</h1>
+                <Link href="/">
+                    <a className="styledLink">
+                        <BsArrowLeftShort />
+                        Leave the cat be
+                    </a>
+                </Link>
+            </div>
+        </>
+    );
+}
