c69efd2d4d57197c8d571781d27911e8ce96eea2
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-11-14 17:02:36 +0530

    added 404 cat image and updated the SEO page

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -3,129 +3,126 @@ import { useRouter } from "next/router";
 
 import config from "config.json";
 
-const siteMetaData = {
-    title: config.title,
-    siteUrl: config.siteUrl,
-    twitter: config.user.twitterLink,
-    socialImage: config.socialImage,
-    author: config.username,
+const siteMetadata = {
+  title: config.title,
+  siteUrl: config.siteUrl,
+  twitter: config.user.twitterLink,
+  socialImage: config.socialImage,
+  author: config.username,
 };
-
 
 const CommonSEO = ({ title, description, ogType, ogImage, twImage }) => {
-    const router = useRouter();
-  
-    return (
-      <>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta
-          property="og:url"
-          content={`${siteMetaData.siteUrl}${router.asPath}`}
-        />
-        <meta property="og:type" content={ogType} />
-        <meta property="og:site_name" content={siteMetaData.title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:title" content={title} />
-        <meta property="og:image" content={ogImage} key={ogImage} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content={siteMetaData.twitter} />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={twImage} />
-      </>
-    );
-  };
+  const router = useRouter();
 
-
-export const pageSEO = ({
-    title, description
-}) => {
-    const ogImageUrl = siteMetaData.siteUrl + siteMetaData.socialImage;
-    const twImageUrl = siteMetaData.siteUrl + siteMetaData.socialImage;
-
-    return (
-        <CommonSEO
-            title={title}
-            description={description}
-            ogType="website"
-            ogImage={ogImageUrl}
-            twImage={twImageUrl}
-        />
-    );
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta
+        property="og:url"
+        content={`${siteMetadata.siteUrl}${router.asPath}`}
+      />
+      <meta property="og:type" content={ogType} />
+      <meta property="og:site_name" content={siteMetadata.title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:image" content={ogImage} key={ogImage} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content={siteMetadata.twitter} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={twImage} />
+    </Head>
+  );
 };
 
-export const blogSEO = ({
-    title, summary, date, slug
-}) => {
-    const publishedAt = new Date(date).toISOString();
-    const featuredImage = CloudinaryMetaImageUrl({ title });
+export const PageSEO = ({ title, description }) => {
+  const ogImageUrl = siteMetadata.siteUrl + siteMetadata.socialImage;
+  const twImageUrl = siteMetadata.siteUrl + siteMetadata.socialImage;
 
-    return (
-        <>
-            <CommonSEO
-                title={title}
-                description={summary}
-                ogType="article"
-                ogImage={featuredImage}
-                twImage={featuredImage}
-            />
-            <>
-                {date && (
-                    <meta property="article:published_time" content={publishedAt} />
-                )}
-            </>
-        </>
-    );
+  return (
+    <CommonSEO
+      title={title}
+      description={description}
+      ogType="website"
+      ogImage={ogImageUrl}
+      twImage={twImageUrl}
+    />
+  );
 };
 
+export const BlogSEO = ({ title, summary, date, slug }) => {
+  const publishedAt = new Date(date).toISOString();
+  const featuredImage = CloudinaryMetaImageUrl({ title });
+
+  return (
+    <>
+      <CommonSEO
+        title={title}
+        description={summary}
+        ogType="article"
+        ogImage={featuredImage}
+        twImage={featuredImage}
+      />
+      <Head>
+        {date && (
+          <meta property="article:published_time" content={publishedAt} />
+        )}
+      </Head>
+    </>
+  );
+};
+
+// thanks to: https://braydoncoyer.dev/blog/how-to-dynamically-create-open-graph-images-with-cloudinary-and-next.js
 export const CloudinaryMetaImageUrl = ({
-    title,
-    cloudName = "dhoygnjc7",
-    imagePublicId = "blog_banner.png",
-    cloudinaryUrlBase = "https://res.cloudinary.com",
-    version = "v1641893609",
-    titleFont = "roboto",
-    titleExtraConfig = "_bold",
-    textColor = "BBBBBB",
-    textAreaWidth = 600,
-    textAreaHeight = 430,
-    titleFontSize = 40,
-    imageWidth = 1200,
-    imageHeight = 630,
-    textBottomOffset = -40,
-    textLeftOffset = 290,
-  }): string => {
-    const imageConfig = [
-        `w_${imageWidth}`,
-        `h_${imageHeight}`,
-        `c_fill`,
-        `f_auto`,
-    ].join(",");
+  title,
+  cloudName = "dhoygnjc7",
+  imagePublicId = "blog_banner.png",
+  cloudinaryUrlBase = "https://res.cloudinary.com",
+  version = "v1641893609",
+  titleFont = "roboto",
+  titleExtraConfig = "_bold",
+  textColor = "BBBBBB",
+  textAreaWidth = 600,
+  textAreaHeight = 430,
+  titleFontSize = 40,
+  imageWidth = 1200,
+  imageHeight = 630,
+  textBottomOffset = -40,
+  textLeftOffset = 290,
+}): string => {
+  const imageConfig = [
+    `w_${imageWidth}`,
+    `h_${imageHeight}`,
+    "c_fill",
+    "f_auto",
+  ].join(",");
 
-    const titleConfig = [
-        `w_${textAreaWidth}`,
-        `h_${textAreaHeight}`,
-        `c_fit`,
-        `co_rgb:${textColor}`,
-        `y_${textBottomOffset}`,
-        `x_${textLeftOffset}`,
-        `l_text:${titleFont}_${titleFontSize}${titleExtraConfig}:${encodeURIComponent(
-            title
-        )}`,
-    ].join(",");
+  const titleConfig = [
+    `w_${textAreaWidth}`,
+    `h_${textAreaHeight}`,
+    "c_fit",
+    `co_rgb:${textColor}`,
+    `y_${textBottomOffset}`,
+    `x_${textLeftOffset}`,
+    `l_text:${titleFont}_${titleFontSize}${titleExtraConfig}:${encodeURIComponent(
+      title
+    )}`,
+  ].join(",");
 
-    const urlParts = [
-        cloudinaryUrlBase,
-        cloudName,
-        "image",
-        "upload",
-        imageConfig,
-        titleConfig,
-        version,
-        imagePublicId,
-    ];
+  const urlParts = [
+    cloudinaryUrlBase,
+    cloudName,
+    "image",
+    "upload",
+    imageConfig,
+    titleConfig,
+    version,
+    imagePublicId,
+  ];
 
-    return urlParts.join("/");
+  // "https://res.cloudinary.com/dbsdoq31k/image/upload/w_1200,h_630,c_fit,co_rgb:FFFFFF,y_-40,l_text:arial_60_bold:The%20visitor%20design%20pattern/v1641893609/blog_banner.png";
+  // https://res.cloudinary.com/dbsdoq31k/image/upload/w_600,h_430,c_fit,co_rgb:BBBBBB,y_-40,x_290,l_text:roboto_50_bold:The%20visitor%20design%20pattern/v1641893609/blog_banner.png
 
-  }
+  return urlParts.join("/");
+};
