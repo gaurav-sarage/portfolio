5ef43345ca418867159ebf1c920f3c3346549c46
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-11-17 23:36:34 +0530

    added functionality to create a new markdown file for a blog

---
+++ utils/createPost.js
@@ -0,0 +1,27 @@
+// script to create an empty post that is already filled with frontmatter
+
+const path = require("path");
+const fs = require("fs");
+
+const fileName = process.argv[2] || "default";
+const filePath = path.resolve(".", "posts", `${fileName}.mdx`);
+
+const frontMatter = `---
+title: "${fileName}"
+description: ""
+date: ${new Date().toISOString()}
+tags: []
+cover:
+    image: "/blogImages/"
+    alt: ""
+    caption: ""
+draft: true
+showToc: true
+---`;
+
+function createPost(path, frontMatter) {
+    fs.writeFileSync(path, frontMatter);
+    console.log(`new post ${filePath} created.`);
+}
+
+createPost(filePath, frontMatter);
