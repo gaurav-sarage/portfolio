2267c7f7f47e1e223b6a6245c346965c5522f8f7
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-11-20 22:43:53 +0530

    implemented nextjs ssr for dynamic blog posts with mdx rendering, seo optimzation & nav components

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,92 @@
+import { GetStaticProps, GetStaticPaths } from "next";
+import { MDXRemote } from "next-mdx-remote";
+
+import { gertAllPostIds, getPostData } from "@/utils/getPosts";
+import { generateMdx, getToc } from "@/utils/mdxUtilities";
+import BlogContainer from "@/components/React/Blog/BlogContainer";
+import { BlogSEO } from "@/components/SEO";
+import Pre from "@/components/MDX/Pre";
+import Blockquote from "@/components/MDX/Blockquote";
+import HiddenExpand from "@/components/MDX/HiddenExpand";
+import StyledAnchor from "@/components/MDX/StyledAnchor";
+import CustomOl from "@/components/MDX/Lists/ol";
+import CustomUl from "@/components/MDX/Lists/ul";
+import Callout from "@/components/MDX/Callout";
+import BasicCard from "@/components/MDX/Card/BasicCard";
+import CardWithTitle from "@/components/MDX/Card/CardWithTitle";
+import CodeBlock from "@/components/MDX/Codeblock";
+import SeparatorSvg from "@/components/React/SeparatorSvg";
+import NextPrevArticles from "@/components/React/Blog/NextPrevArticles";
+
+const components = {
+    pre: (props) => <Pre {...props} />,
+    blockquote: (props) => <Blockquote {...props} />,
+    a: (props) => <StyledAnchor {...props} />,
+    hr: (props) => <SeparatorSvg {...props} />,
+    ol: (props) => <CustomOl {...props} />,
+    ul: (props) => <CustomUl {...props} />,
+    code: (props) => (
+        <CodeBlock
+            filename={props.filename}
+            highlight={props.highlight}
+            {...props}
+        />
+    ),
+    HiddenExpand,
+    Callout,
+    BasicCard,
+    CardWithTitle,
+};
+
+export default function BlogPost({
+    frontMatter,
+    source,
+    slug,
+    tableOfContents,
+    recommendedPostList
+}) {
+    return (
+        <>
+            <BlogSEO
+                title={frontMatter.title}
+                summary={frontMatter.description}
+                date={frontMatter.date}
+                slug={slug}
+            />
+            <BlogContainer
+                frontMatter={frontMatter}
+                tableOfContents={tableOfContents}
+                slug={slug}
+            >
+                <MDXRemote {...source} components={components} />
+            </BlogContainer>
+            <NextPrevArticles recommendedPostList={recommendedPostList} />
+        </>
+    );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const paths = gertAllPostIds();
+    return {
+        paths,
+        fallback: false
+    };
+};
+
+export const getStaticProps: GetStaticProps = async ({
+    params
+}) => {
+    const { content, frontMatter, slug, recommendedPostList } = await getPostData(
+        params.slug as string
+    );
+
+    return {
+        props: {
+            source: await generateMdx(frontMatter, content),
+            frontMatter,
+            slug,
+            tableOfContents: await getToc(frontMatter, content),
+            recommendedPostList,
+        },
+    };
+};
