7a5f13cf26ecd0176a75d934e373208ab718c6e6
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-11-22 00:41:31 +0530

    refactored pre component to utilize css modules for styling

---
+++ components/MDX/Pre/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./style.module.css";
+
+export default function Pre({ ...props }) {
+    return (
+        <div className={styles.container}>
+            <pre
+                {...props}
+                className={`${props.className ? props.className : ""} ${styles.pre}`}
+            />
+        </div>
+    );
+}
