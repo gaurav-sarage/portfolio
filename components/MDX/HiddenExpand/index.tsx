8d7f92695515fa4aae1bb601d8a4f18922fdf823
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-11-24 21:12:15 +0530

    introduced HiddenExpand component with styles from style.module.css

---
+++ components/MDX/HiddenExpand/index.tsx
@@ -0,0 +1,14 @@
+import styles from "./style.module.css";
+
+export default function HiddenExpand({ summary, children }) {
+    return (
+        <details className={styles.details}>
+            <summary className={styles.summary}>
+                {summary}
+            </summary>
+            <div className={styles.children}>
+                {children}
+            </div>
+        </details>
+    );
+}
